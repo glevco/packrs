87c90eca5d33489f2bf3ad19aa7394c79374dd5b
Gabriel Levcovitz <g.levcovitz@gmail.com>
2024-08-11 23:25:59 -0300

    feat: implement pack

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+mod pack;
 mod unpack;
 
 // TODO: Docs
---
+++ src/pack.rs
@@ -0,0 +1,55 @@
+pub trait Pack {
+    fn pack_into(&self, buf: &mut Vec<u8>);
+
+    fn pack(&self) -> Vec<u8> {
+        let mut buf = vec![];
+        self.pack_into(&mut buf);
+        buf
+    }
+}
+
+impl Pack for &[u8] {
+    fn pack_into(&self, buf: &mut Vec<u8>) {
+        buf.extend(self.iter());
+    }
+}
+
+impl Pack for &str {
+    fn pack_into(&self, buf: &mut Vec<u8>) {
+        buf.extend(self.bytes());
+    }
+}
+
+impl Pack for Vec<u8> {
+    fn pack_into(&self, buf: &mut Vec<u8>) {
+        buf.extend(self);
+    }
+}
+
+impl Pack for String {
+    fn pack_into(&self, buf: &mut Vec<u8>) {
+        buf.extend(self.bytes());
+    }
+}
+
+impl<P: Pack> Pack for Vec<P> {
+    fn pack_into(&self, buf: &mut Vec<u8>) {
+        for item in self {
+            item.pack_into(buf)
+        }
+    }
+}
+
+impl<const N: usize> Pack for [u8; N] {
+    fn pack_into(&self, buf: &mut Vec<u8>) {
+        buf.extend(self)
+    }
+}
+
+impl<const N: usize, P: Pack> Pack for [P; N] {
+    fn pack_into(&self, buf: &mut Vec<u8>) {
+        for item in self {
+            item.pack_into(buf)
+        }
+    }
+}
